2590df5f8d6dc0f721af574e29a45a2e00e71c50
Ederson <ederson@Ederson-Note>
2017-04-19 16:55:58 -0300

    Projeto inicial em branco

---
+++ Seminario/app/src/main/java/seminario/aet/compmovel/ime/usp/br/seminrio/MainActivity.java
@@ -0,0 +1,13 @@
+package seminario.aet.compmovel.ime.usp.br.seminrio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
