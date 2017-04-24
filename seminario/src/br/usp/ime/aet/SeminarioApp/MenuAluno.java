6f3bfc62534b3aedba07186dd192f890ff32b886
Taiane Ramos <exhora.tat@gmail.com>
2017-04-24 15:12:11 -0300

    Arquivos criados para o menu do professor e aluno

---
+++ seminario/src/br/usp/ime/aet/SeminarioApp/MenuAluno.java
@@ -0,0 +1,24 @@
+package br.usp.ime.aet.SeminarioApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+public class MenuAluno extends Activity {
+
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.aluno_menu);
+   }
+   
+   public void alterarCadastro(View v) {
+
+   }
+
+   public void listarSeminarios(View v){
+
+   }
+   
+}
---
+++ seminario/src/br/usp/ime/aet/SeminarioApp/MenuProf.java
@@ -0,0 +1,29 @@
+package br.usp.ime.aet.SeminarioApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+public class MenuProf extends Activity {
+
+   @Override
+   public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.prof_menu);
+   }
+   
+   public void alterarCadastro(View v) {
+
+   }
+ 
+   public void cadastrarSeminario(View v) {
+
+   }
+
+
+   public void listarSeminarios(View v){
+
+   }
+   
+}
